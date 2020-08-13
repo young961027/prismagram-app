a64577d42917cf3c42761b2c6311a72acfb95fd8
loopbackseal <young961027@gmail.com>
2020-08-13 20:59:09 +0900

    Tabs Navigation

---
+++ navigation/TabNavigation.js
@@ -0,0 +1,24 @@
+import { View } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import Home from "../screens/Home";
+import Notification from "../screens/Notification";
+import Profile from "../screens/Profile";
+import Search from "../screens/Search";
+
+const TabNavigation = createBottomTabNavigator({
+  Home,
+  Notification,
+  Add: {
+    screen: View,
+    navigationOptions: {
+      tabBarOnPress: () => {
+        console.log("Add");
+      },
+    },
+  },
+  Profile,
+  Search,
+});
+
+export default createAppContainer(TabNavigation);
